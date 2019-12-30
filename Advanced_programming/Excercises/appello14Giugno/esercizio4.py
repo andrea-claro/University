ab5f7692caf117f4ce3ed7a0b78eabcd6f5d3f8f
Andrea <andreaclaro.04@gmail.com>
2019-12-30 18:21:46 +0100

    Inserito esercizio multiprocessing

--- Advanced_programming/Excercises/appello14Giugno/esercizio4.py
+++ Advanced_programming/Excercises/appello14Giugno/esercizio4.py
@@ -4,7 +4,61 @@
 # e il parametro concorrenza. La funzione procTesto deve cecare nel testo dell’i-esimo file
 # della prima lista l’i-esima stringa della seconda lista e deve fare cio` con un processo
 # separato per ogni coppia file-stringa. Attenzione, il callable utilizzato da procTesto
-# deve riceve in input il testo del file e non il file. La funzione procTesto deve inoltre
+# deve ricevere in input il testo del file e non il file. La funzione procTesto deve inoltre
 # stampare per ciascuna coppia file-stringa se la stringa e` presente o meno nel file.
 # Le stampe devono avvenire nell’ordine in cui terminano i processi e al termine degli stessi.
 
+import multiprocessing
+
+def precTesto(fileParole=None, lstNomiFile=[]):
+    with open(fileParole, "r") as ff:
+        texto = ff.read().split()
+        for i in range(0, len(lstNomiFile)):
+            with open(lstNomiFile[i], "r") as f:
+                text = f.read().split()
+                confronto(texto, text, i)
+
+def confronto(textParole, textFile, n):
+    if textParole[n] in textFile:
+        print("Parola presente")
+    else:
+        print("Parola non presente")
+            
+print("---------------------------SENZA CONCORRENZA--------------------------------")            
+precTesto("appello14Giugno/parole", ["appello14Giugno/testox", "appello14Giugno/testox-1", "appello14Giugno/testox-2"])
+
+
+
+
+
+
+
+
+
+
+def procTesto(concorrenza=False, fileParole=None, lstNomiFile=[]):
+    with open(fileParole, "r") as f:
+        parole = f.read().split()
+    create_processes(concorrenza, parole, lstNomiFile)
+
+def create_processes(concorrenza=False, parole=None, lstNomiFile=[]):
+    if concorrenza is True:
+        for i in range(0, len(lstNomiFile)):
+            print(i)
+            process = multiprocessing.Process(target=worker, name="Pippo", args=(i, parole, lstNomiFile[i]))
+            process.daemon = True
+            process.start()
+            process.join()
+    else:
+        print("NO CONCORRENZA")
+
+def worker(i=-1, parole=None, file=None):
+    with open(file, "r") as f:
+        testo = f.read().split()
+    if parole[i] in testo:
+        print("Associazione presente {}-{}".format(parole[i], testo))
+    else:
+        print("Parola non presente {}-{}".format(parole[i], testo))
+
+print("---------------------------CONCORRENZA--------------------------------")            
+procTesto(True ,"appello14Giugno/parole", ["appello14Giugno/testox", "appello14Giugno/testox-1", "appello14Giugno/testox-2"])
