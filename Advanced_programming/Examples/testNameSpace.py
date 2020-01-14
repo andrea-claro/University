1113a3e0f88c1d574c34b7d421b000130c61bf88
Andrea <andreaclaro.04@gmail.com>
2020-01-14 17:17:15 +0100

    Name space test added

---
+++ Advanced_programming/Examples/testNameSpace.py
@@ -0,0 +1,34 @@
+class A:
+
+    def foo():
+        print("dio cane")
+
+a = 4
+b = 'zaino'
+c = A()
+d = A
+f = c.foo
+
+print("dir di a")
+print(dir(a))
+print()
+print("dir di b")
+print(dir(b))
+print()
+print("dir di c")
+print(dir(c))
+print()
+print("vars di c")
+print(vars(c))
+print()
+print("dir di d")
+print(dir(d))
+print()
+print("vars di d")
+print(vars(d))
+print()
+print("dir di f")
+print(dir(f))
+print()
+print("vars di f")
+print(vars(f))
